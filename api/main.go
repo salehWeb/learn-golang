68e21124e7362ab2c32d2cadc5e363486d467c9a
salehWeb <salehwebdev2004@gmail.com>
2023-01-06 15:27:14 +0200

    CRUD DataBase System

--- api/main.go
+++ api/main.go
@@ -5,42 +5,77 @@ import (
 )
 
 type ToDo struct {
-	Id      uint32 `json:"id"`
+	Id      uint8  `json:"id"`
 	Content string `json:"content"`
 	IsDone  bool   `json:"isDone"`
 }
 
 type Db interface {
 	InsertTodo(content string)
-	GetToDos() []ToDo
+	GetToDos() map[uint8]ToDo
+	DeleteItemByID(id uint8) bool
+	UpdateItemByID(id uint8, content string, isDone bool) bool
 }
 
 type db struct {
-	Len   uint32
-	ToDos []ToDo
+	Len   uint8          `json:"length"`
+	ToDos map[uint8]ToDo `json:"toDos"`
 }
 
 func NewDb() Db {
+
 	return &db{
 		Len:   0,
-		ToDos: []ToDo{},
+		ToDos: make(map[uint8]ToDo),
 	}
+}
+
+func (db *db) GetToDos() map[uint8]ToDo {
+	return db.ToDos
 }
 
 func (db *db) InsertTodo(content string) {
 
+	if db.Len == 254 {
+		return
+	}
+
+	var id uint8 = db.Len + 1
 	var newTodo ToDo = ToDo{
-		Id:      db.Len + 1,
+		Id:      id,
 		Content: content,
 		IsDone:  false,
 	}
 
-	db.ToDos = append(db.ToDos, newTodo)
+	db.ToDos[id] = newTodo
 	db.Len += 1
 }
 
-func (db *db) GetToDos() []ToDo {
-	return db.ToDos
+func (db *db) DeleteItemByID(id uint8) bool {
+	_, ok := db.ToDos[id]
+
+	if ok {
+		delete(db.ToDos, id)
+		return true
+	} else {
+		return false
+	}
+
+}
+
+func (db *db) UpdateItemByID(id uint8, content string, isDone bool) bool {
+
+	if item, ok := db.ToDos[id]; ok {
+
+		item.Content = content
+		item.IsDone = isDone
+
+		db.ToDos[id] = item
+		return true
+	} else {
+		return false
+	}
+
 }
 
 func main() {
@@ -50,9 +85,42 @@ func main() {
 	DB.InsertTodo("Hello World 2")
 	DB.InsertTodo("Hello World 3")
 
-	toDos := DB.GetToDos()
+	var ToDos map[uint8]ToDo
+	ToDos = DB.GetToDos()
+	ReadTodos(&ToDos)
 
-	for _, v := range toDos {
+	var isSuccess1 bool = DB.DeleteItemByID(1)
+
+	if isSuccess1 {
+		fmt.Println("Success Deleting Todo")
+		fmt.Println("Todos After Deleting")
+
+		ToDos = DB.GetToDos()
+
+		ReadTodos(&ToDos)
+
+	} else {
+		fmt.Println("Failed to Delete Todo")
+	}
+
+	var isSuccess2 bool = DB.UpdateItemByID(3, "World Hello Updated", true)
+
+	if isSuccess2 {
+		fmt.Println("Success Updating Todo")
+		fmt.Println("Todos After Updating")
+
+		ToDos = DB.GetToDos()
+
+		ReadTodos(&ToDos)
+
+	} else {
+		fmt.Println("Failed to Update Todo")
+	}
+
+}
+
+func ReadTodos(toDos *map[uint8]ToDo) {
+	for _, v := range *toDos {
 		fmt.Printf("Todos Id: %d Content is %s and IsDone %t\n\n", v.Id, v.Content, v.IsDone)
 	}
 }
